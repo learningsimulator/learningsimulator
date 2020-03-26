13cf1536ae81878be10abc39ce256c7c7c637bbf
Markus Jonsson <markusrobertjonsson@gmail.com>
2020-03-26 16:05:56 +0100

    Adding a test of trace for SR.

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -13,96 +13,125 @@ class TestBasic(unittest.TestCase):
         plt.close('all')
 
     def test_sr(self):
-        pass
-#         script = '''
-# n_subjects            : 200
-# mechanism             : sr
-# behaviors             : approach,eat,other
-# stimulus_elements     : plant,berry,sugar,no_reward
-# response_requirements : approach:plant, eat:berry
-# start_v               : plant->other:0, berry->other:0, default:-1
-# alpha_v               : 0.1
-# alpha_w               : 0.1
-# beta                  : 1
-# behavior_cost         : approach:1, default: 0
-# u                     : sugar:10, default:0
-# bind_trials           : off
+        script = '''
+n_subjects           : 150
+mechanism            : SR
+behaviors            : approach, eat, other
+stimulus_elements    : plant, berry, sugar, no_reward
+response_requirements: approach:plant, eat:berry
+start_v              : plant->other:0, berry->other:0, default:-1
+alpha_v              : 0.1
+alpha_w              : 0.1
+beta                 : 1
+behavior_cost        : approach:1, default: 0
+u                    : sugar:10, default:0
+bind_trials          : off
 
-# @phase acquisition stop: plant=200
-# new_trial                 | PLANT
-# PLANT          plant      | approach: BERRY    |  new_trial
-# BERRY          berry      | eat: REWARD        |  NO_REWARD
-# REWARD         sugar      | new_trial
-# NO_REWARD      no_reward  | new_trial
+@phase acquisition stop: plant=300
+new_trial             | PLANT
+PLANT       plant     | approach: BERRY | new_trial
+BERRY       berry     | eat: REWARD     | NO_REWARD
+REWARD      sugar     | new_trial
+NO_REWARD   no_reward | new_trial
 
-# @run acquisition runlabel:notrace
+trace: 0
+@run acquisition runlabel:0
 
-# trace: 0
-# @run acquisition runlabel:trace=0
+trace: 0.25
+@run acquisition runlabel:0.25
 
-# trace: 0.2
-# @run acquisition runlabel:trace=0.2
+trace: 0.5
+@run acquisition runlabel:0.5
 
-# trace: 0.4
-# @run acquisition runlabel:trace=0.4
+trace: 0.75
+@run acquisition runlabel:0.75
 
-# trace: 0.6
-# @run acquisition runlabel:trace=0.6
+xscale: plant
+subject: average
 
-# trace: 0.8
-# @run acquisition runlabel:trace=0.8
+runlabel:0
+@vplot berry->eat {'label':'0'}
+@vplot plant->approach {'label':'0: plant->approach'}
 
-# trace: 1
-# @run acquisition runlabel:trace=1
+runlabel:0.25
+@vplot berry->eat {'label':'0.25'}
 
-# xscale: plant
-# subject: average
+runlabel:0.5
+@vplot berry->eat {'label':'0.5'}
+@vplot plant->approach {'label':'0.5: plant->approach'}
 
-# runlabel: notrace
-# @pplot plant->approach {'label':'notrace'}
+runlabel:0.75
+@vplot berry->eat {'label':'0.75'}
 
-# runlabel: trace=0
-# @pplot plant->approach {'label':'trace=0'}
+@legend
+'''
+        script_obj, script_output = run(script)
+        plot_data = get_plot_data()
 
-# runlabel: trace=0.2
-# @pplot plant->approach {'label':'trace=0.2'}
+        # Check that the start values (x=0) are at (0,-1)
+        for _, plot in plot_data.items():
+            self.assertEqual(plot['x'][0], 0)
+            self.assertEqual(plot['y'][0], -1)
 
-# runlabel: trace=0.4
-# @pplot plant->approach {'label':'trace=0.4'}
+        # Check that all y-values are between -1 and 10
+        for _, plot in plot_data.items():
+            for y in plot['y']:
+                self.assertLessEqual(y, 10)
+                self.assertGreaterEqual(y, -1)
 
-# runlabel: trace=0.6
-# @pplot plant->approach {'label':'trace=0.6'}
+        # Check that all curves are "almost" nondecreasing
+        for _, plot in plot_data.items():
+            y_values = plot['y']
+            for i in range(1, len(y_values)):
+                if y_values[i] < y_values[i - 1]:
+                    self.assertLess(y_values[i - 1] - y_values[i], 0.05)
 
-# runlabel: trace=0.8
-# @pplot plant->approach {'label':'trace=0.8'}
+        # Check that "0: plant->approach" is constant -1 (the start_v value)
+        ypa0 = plot_data['0: plant->approach']['y']
+        for y in ypa0:
+            self.assertEqual(y, -1)
 
-# runlabel: trace=1
-# @pplot plant->approach {'label':'trace=1'}
+        # Check that "0.5: plant->approach" converges to a value between -0.5 and 0
+        ypa50 = plot_data['0.5: plant->approach']['y']
+        for y in ypa50:
+            self.assertLess(y, 0)
+        ypa50_last = ypa50[-50:-1]  # The last 50 items, exept the last
+        for y in ypa50_last:
+            self.assertGreater(y, -0.5)
+        self.assertLess(max(ypa50_last) - min(ypa50_last), 0.02)
 
-# @legend
-# '''
-#         script_obj, script_output = run(script)
-#         plot_data = get_plot_data()
+        # Check that "0" converges to 10
+        y0 = plot_data['0']['y']
+        for y in y0:
+            self.assertLessEqual(y, 10)
+        y0_last = y0[-50:-1]
+        for y in y0_last:
+            self.assertGreater(y, 9.8)
+        self.assertLess(max(y0_last) - min(y0_last), 0.05)
 
-#         # Check that the plot for notrace and trace=0 are the same
-#         self.assertEqual(plot_data['notrace']['x'], plot_data['trace=0']['x'])
-#         self.assertEqual(len(plot_data['notrace']['y']), len(plot_data['trace=0']['y']))
-#         for yno, y0 in zip(plot_data['notrace']['y'], plot_data['trace=0']['y']):
-#             self.assertAlmostEqual(yno, y0, places=4)
+        # Check that "0.25" converges to a value between 9 and 9.5
+        y25 = plot_data['0.25']['y']
+        for y in y25:
+            self.assertLess(y, 9.5)
+        y25_last = y25[-50:-1]
+        for y in y25_last:
+            self.assertGreater(y, 9)
+        self.assertLess(max(y25_last) - min(y25_last), 0.01)
 
-#         # Store all pplot end-values in end_value
-#         end_value = dict()
-#         for lbl in plot_data:
-#             end_value[lbl] = plot_data[lbl]['y'][-1]
-#             self.assertLessEqual(end_value[lbl], 1)
-#             self.assertGreaterEqual(end_value[lbl], 0)
+        # Check that "0.50" converges to a value between 3 and 4
+        y50 = plot_data['0.5']['y']
+        for y in y50:
+            self.assertLess(y, 7)
+        y50_last = y50[-50:-1]
+        for y in y50_last:
+            self.assertGreater(y, 6)
+        self.assertLess(max(y50_last) - min(y50_last), 0.1)
 
-#         # Check that the start values (x=0) are all the same
-
-
-#         # Check that the end-values increase with trace
-#         self.assertLess(end_value['trace=0'], end_value['trace=0.2'])
-#         self.assertLess(end_value['trace=0.2'], end_value['trace=0.4'])
-#         self.assertLess(end_value['trace=0.4'], end_value['trace=0.6'])
-#         self.assertLess(end_value['trace=0.6'], end_value['trace=0.8'])
-#         self.assertLess(end_value['trace=0.8'], end_value['trace=1'])
+        # Check that "0.75" converges to a value between 3 and 4
+        y75 = plot_data['0.75']['y']
+        for y in y75:
+            self.assertLess(y, 4)
+        y75_last = y75[-50:-1]
+        for y in y75_last:
+            self.assertGreater(y, 3)
+        self.assertLess(max(y75_last) - min(y75_last), 0.2)
