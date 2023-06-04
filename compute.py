0afe09d0059947ada3382590c5a38c6b9032b492
Stefano Ghirlanda <drghirlanda@gmail.com>
2023-06-04 09:30:59 -0400

    fixed whitespace

--- compute.py
+++ compute.py
@@ -1,14 +1,13 @@
 import multiprocessing
 
-def worker( q ):
+def worker(q):
     while True:
         script_obj = q.get()
         simulation_data = script_obj.run()
-        q.put( simulation_data )
+        q.put(simulation_data)
+
 
 worker_queue = multiprocessing.Queue()
 progress_queue = multiprocessing.Queue()
-process = multiprocessing.Process( target=worker, args=(worker_queue,) )
+process = multiprocessing.Process(target=worker, args=(worker_queue,))
 process.start()
-
-
