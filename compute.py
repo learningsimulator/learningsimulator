ddb84507edf7877c306992657800298f4c05f846
Stefano Ghirlanda <drghirlanda@gmail.com>
2023-06-06 13:24:32 -0400

    no longer starting compute.process automatically. wrapped simulation in try/except, with exceptions forwarded to caller process

--- compute.py
+++ compute.py
@@ -1,13 +1,16 @@
 import multiprocessing
+import traceback
 
 def worker(q):
     while True:
-        script_obj = q.get()
-        simulation_data = script_obj.run()
-        q.put(simulation_data)
-
+        try:
+            script_obj = q.get()
+            simulation_data = script_obj.run()
+            q.put((simulation_data, ))
+        except Exception as ex:
+            q.put((ex, traceback.format_exc()))
 
 worker_queue = multiprocessing.Queue()
 progress_queue = multiprocessing.Queue()
 process = multiprocessing.Process(target=worker, args=(worker_queue,))
-process.start()
+
