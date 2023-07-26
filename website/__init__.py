9a6d07c037b1261a63e71617e3e9b5afeaec52e1
Markus Jonsson <markusrobertjonsson@gmail.com>
2023-07-26 15:54:56 +0200

    Test deploy commit.

--- website/__init__.py
+++ website/__init__.py
@@ -22,7 +22,13 @@ def create_app():
     # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     # The MySQL db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:hejsan123@localhost/db_weblesim'
+    SECRET_DB_USER = "birds"  # XXX Put these secret things in a config.py or similar
+    SECRET_DB_PW = "xBqjfrF.9H6BT4G"
+    SECRET_DB_NAME = f"{SECRET_DB_USER}@default"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{SECRET_DB_USER}:{SECRET_DB_PW}@mysql.server/{SECRET_DB_NAME}'
+
+    # When running locally:
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:hejsan123@localhost/db_weblesim'
 
     # To avoid warning. We do not use the Flask-SQLAlchemy event system, anyway.
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
